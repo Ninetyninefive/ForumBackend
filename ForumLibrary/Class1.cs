58a907a9a411ec8547ec5e721a08c9a2c34ae4cd
NinetynineFive <casper.jaller@gmail.com>
2020-12-16 16:25:34 +0100

    Restructure

---
+++ ForumLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+using Dapper;
+
+namespace ForumLibrary
+{
+    public class Class1
+    {
+        private void SayHello()
+        {
+            var message = "hello";
+        }
+    }
+}
